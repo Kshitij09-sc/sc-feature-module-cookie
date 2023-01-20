8212aee149c0a79094e1384ca842b99d44176884
Kshitij Patil <kshitijpatil@sharechat.co>
2023-01-21 02:48:18 +0530

    Move operation reordering
    Signed-off-by: Kshitij Patil <kshitijpatil@sharechat.co>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import os
 import shutil
 
-MAIN_SOURCE_SET = "src/main/java"
-ANDROID_TEST_SOURCE_SET = "src/androidTest/java"
-TEST_SOURCE_SET = "src/test/java"
+MAIN_SOURCE_SET = Path("src/main/java")
+ANDROID_TEST_SOURCE_SET = Path("src/androidTest/java")
+TEST_SOURCE_SET = Path("src/test/java")
 PACKAGE_NAME = '{{ cookiecutter.package_name }}'
 MODULE_NAME = '{{ cookiecutter.module_name }}'
 
@@ -18,20 +18,23 @@ def expand_module():
     old_root = Path(os.path.realpath(os.path.curdir))
     new_root = MODULE_NAME[1:] if MODULE_NAME.startswith(":") else MODULE_NAME
     new_root = old_root.parent/new_root.replace(":","/")
-    shutil.move(old_root, new_root)
-    os.chdir(old_root.parent)
-    return new_root
+    new_root.mkdir(parents=True, exist_ok=True)
+    for file in os.listdir(old_root):
+        src_path = old_root/file
+        dst_path = new_root/file
+        shutil.move(src_path, dst_path)
+    old_root.rmdir()
+    
 
 def make_settings_entry():
-    settings_gradle_path = Path(f"{os.getcwd()}/settings.gradle.kts")
+    settings_gradle_path = Path("../settings.gradle.kts")
     if settings_gradle_path.exists():
         with open(settings_gradle_path, "a") as settings_file:
             settings_file.writelines(f"includeIfEnabled(\"{MODULE_NAME}\")\n")
 
 if __name__ == "__main__":
-    new_root = expand_module()
-    expand_package(new_root/MAIN_SOURCE_SET)
-    expand_package(new_root/ANDROID_TEST_SOURCE_SET)
-    expand_package(new_root/TEST_SOURCE_SET)
     make_settings_entry()
-    
+    expand_package(MAIN_SOURCE_SET)
+    expand_package(ANDROID_TEST_SOURCE_SET)
+    expand_package(TEST_SOURCE_SET)
+    expand_module()
